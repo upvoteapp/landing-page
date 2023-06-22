4ffbab4992a78f9d998b5b37009cb929e6b3838b
Defy <61808223+De-y@users.noreply.github.com>
2023-06-21 20:39:53 -0700

    Landing page, :white_check_mark:
    The landing page is finally done, it just depends on what the project will be focusing on, will it be hosting, or will it be a prototype for a better reddit?

---
+++ src/views/AboutView.vue
@@ -0,0 +1,52 @@
+<template>
+  <main>
+    <br />
+    <h1>About Upvote</h1>
+    <br /> <br />
+    <div class="cemter">
+    <p class="wrap" style="font-size: small;">Upvote is an attempt of making the fastest & most powerful machine control panel, allowing you to be able to have the same functionality as Pterodactyl being based on a custom engine, all written in the famous language, javascript. If you are interested in contributing to the project, click on the Github button below to be redirected to the project, otherwise, see you on the flip side!  </p>
+    <br />
+    <div class="flex justify-center space-x-4">
+      <a href="https://github.com/upvoteapp/upvote" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0.5 px-1.5 rounded">Github</a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0.5 px-1.5 rounded">
+        <RouterLink to="/">Home</RouterLink>
+      </a>
+    </div>
+    <br />
+    </div>
+</main>
+</template>
+<style scoped>
+main {
+  background-color: white;
+  font-family: 'Poppins', sans-serif;
+  position: absolute;
+  top: 50%;
+  font-size: x-large;
+  left: 50%;
+  margin-right: -50%;
+  width: 50%;
+  text-align: center;
+  transform: translate(-50%, -50%);
+
+}
+.center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+.wrap {
+  white-space: pre-wrap;       /* css-3 */
+  white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+  white-space: -pre-wrap;      /* Opera 4-6 */
+  white-space: -o-pre-wrap;    /* Opera 7 */
+  word-wrap: break-word;       /* Internet Explorer 5.5+ */
+
+}
+.text-right {
+  text-align: right;
+  font-size: xx-small;
+  color: gray;
+  margin-right: 1em;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <br />
+    <h1>Welcome to a new hosting panel more powerful than the others.<br /> Welcome to Upvote.</h1>
+    <br /> <br />
+ <div class="flex justify-center space-x-4">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1.5 rounded">
+      <router-link to="/about">About Upvote</router-link>
+    </button>
+    <a href="https://github.com/upvoteapp/upvote" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1.5 rounded">
+      Project Github
+    </a>
+  </div>
+  <br />
+
+  <h5 class="text-right"><span class="server_time"></span>
+   ©2023 aviance.</h5>
+</main>
+</template>
+<style scoped>
+main {
+  background-color: white;
+  font-family: 'Poppins', sans-serif;
+  position: absolute;
+  top: 50%;
+  font-size: x-large;
+  left: 50%;
+  margin-right: -50%;
+  width: 50%;
+  text-align: center;
+  transform: translate(-50%, -50%);
+
+}
+.text-right {
+  text-align: right;
+  font-size: xx-small;
+  color: gray;
+  margin-right: 1em;
+}
+</style>
